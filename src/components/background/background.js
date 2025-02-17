c5baaf70190d6dfb40dc311de75a9443ee927a72
Henry Tran <tloc1@student.mtsac.edu>
2025-02-16 20:04:42 -0800

    Upload information

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -9,7 +9,7 @@ const backgroundData = {
   part3: " where I am pursuing lower-division courses. Next year, I plan to transfer to a four-year university to further my studies as a Computer Science & Electrical Engineering student.",
   line2: "As a back-end developer, I diligently focus on the system's core mechanisms and work to extend its functionality, ensuring efficiency and scalability. Additionally, I am also concentrating on the field of machine learning to become more versatile and better equipped to handle a broader range of tasks at work.",
   line3Part1: "Beyond coding and innovation in engineering,",
-  line3Part2: ", my hideout is often at 24 Hour Fitness, where I relieve stress and work toward becoming a stronger, healthier version of myself.",
+  line3Part2: " my hideout is often at 24 Hour Fitness, where I relieve stress and work toward becoming a stronger, healthier version of myself.",
 };
 const Background = () => (
   <section className="section background">
--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -4,46 +4,9 @@ import "./experience.scss"
 
 const experienceData = [
   {
-    "company": "Talking Yak",
-    "url": "https://talkingyak.com/",
-    "time": "September 2019 - Present",
-    "position": "Consulting Head of Engineering"
-  },
-  {
-    "company": "Ather Energy",
-    "url": "https://www.atherenergy.com/",
-    "time": "Feb 2019 - September 2019",
-    "position": "Engineering Manager"
-  },
-  {
-    "company": "Invaria - Willow",
-    "url": "https://onewillow.com/",
-    "time": "March 2018 - Feb 2019",
-    "position": "Senior Software Engineer"
-  },
-  {
-    "company": "Invaria - Proxfinity",
-    "url": "https://www.proxfinity.com/",
-    "time": "Jan 2016 - March 2018",
-    "position": "Software Engineer"
-  },
-  {
-    "company": "TAAL Tech",
-    "url": "https://www.taaltech.com/",
-    "time": "June 2015 - Jan 2016",
-    "position": "Software Engineer"
-  },
-  {
-    "company": "Virtual Applets",
-    "url": "https://www.crunchbase.com/organization/virtual-applets",
-    "time": "Jan 2013 - June 2015",
-    "position": "Co-Founder"
-  },
-  {
-    "company": "Tringapps - HBO GO",
-    "url": "https://play.google.com/store/apps/details?id=sg.hbo.hbogo&hl=en&gl=US",
-    "time": "Dec 2011 - Jan 2013",
-    "position": "Research Analyst"
+    "company": "Mount San Antonio College",
+    "time": "July 2023 - May 2024",
+    "position": "Undergraduate Student Researcher"
   },
 ]
 
