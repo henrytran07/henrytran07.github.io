f5d432ffd9e16a5c058508b51368bc08fbb74d77
Mario Wijaya <mwijaya3@hotmail.com>
2025-02-18 01:49:37 -0800

    comment out unneeded

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -11,30 +11,30 @@ const Skills = () => (
     <div className="section__title">Skills</div>
     <div className="section__content">
       <div className="skillz">
-        <div className="skillz__category">
-          <div className="skillz__category__label">{skillsData.languagesTitle}</div>
-          <ul>
+        {/* <div className="skillz__category">
+          <div className="skillz__category__label">{skillsData.languagesTitle}</div> */}
+          {/* <ul>
             {skillsData.languages.map((data) => <li className="skillz__category__item">{data}</li>)}
-          </ul>
-        </div>
+          </ul> */}
+        {/* </div> */}
         <div className="skillz__category">
           <div className="skillz__category__label">{skillsData.frameworksTitle}</div>
           <ul>
             {skillsData.frameworks.map((data) => <li className="skillz__category__item">{data}</li>)}
           </ul>
         </div>
-        <div className="skillz__category">
+        {/* <div className="skillz__category">
           <div className="skillz__category__label">{skillsData.toolsTitle}</div>
           <ul>
             {skillsData.tools.map((data) => <li className="skillz__category__item">{data}</li>)}
           </ul>
-        </div>
-        <div className="skillz__category">
+        </div> */}
+        {/* <div className="skillz__category">
           <div className="skillz__category__label">{skillsData.designTitle}</div>
           <ul>
             {skillsData.design.map((data) => <li className="skillz__category__item">{data}</li>)}
           </ul>
-        </div>
+        </div> */}
       </div>
     </div>
   </section>
