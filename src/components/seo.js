002d08009bae65f20b6a4a43a08ea323dd6bfabf
Henry Tran <tloc1@student.mtsac.edu>
2025-09-25 14:03:01 -0700

    Fix seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -19,22 +19,10 @@ const Seo = () => {
       <meta name="robots" content="index, follow" />
       <meta property="og:title" content="Henry Tran | MtSAC" />
       <meta property="og:description" content="Official page of Henry Tran, MtSAC student" />
-      <meta property="og:image" content="path/to/your/image.jpg" /> {/* Ensure a proper image URL */}
-      <meta property="og:url" content="https://henrytran.com" /> {/* Replace with your site URL */}
+      <meta property="og:image" content="./images/emojis/henry-tran.JPG" /> {/* Ensure a proper image URL */}
+      <meta property="og:url" content="https://henrytran07.github.io/" /> {/* Replace with your site URL */}
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Henry Tran" />
-
-      {/* Twitter meta tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="@henrytran" /> {/* Update with your Twitter handle */}
-      <meta name="twitter:title" content="Henry Tran | MtSAC" />
-      <meta name="twitter:description" content="Official page of Henry Tran, MtSAC student" />
-      <meta name="twitter:image" content="path/to/your/twitter-image.jpg" /> {/* Replace with actual image path */}
-
-      {/* Additional meta tags */}
-      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   );
 };
