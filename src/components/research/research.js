769a47f464623f9bcf68a5b323d75efc35a0be1e
Henry Tran <tloc1@student.mtsac.edu>
2025-09-25 14:13:44 -0700

    Fix seo.js (4)

--- src/components/research/research.js
+++ src/components/research/research.js
@@ -1,20 +1,20 @@
-import * as React from "react"
+import * as React from "react";
 import Parser from 'html-react-parser';
-import "./research.scss"
+import "./research.scss";
 
 const researchData = [
   {
     "name": "Feedback Distillation: Teaching Lightweight to Mimic Expert Feedback in Reasoning Tasks",
     "url": "https://github.com/henrytran07/ChessGame",
-    "description": " <br> <a class='small' href='https://github.com/henrytran07/ChessGame'>",
+    "description": "<br> <a class='small' href='https://github.com/henrytran07/ChessGame'>",
     "used": [
-      {"thing": "Python"},
-      {"thing": "Supervised Learning"},
-      {"thing": "Numpy"},
-      {"thing": "Pandas"},
+      { "thing": "Python" },
+      { "thing": "Supervised Learning" },
+      { "thing": "Numpy" },
+      { "thing": "Pandas" },
     ]
   },
-]
+];
 
 const OtherResearch = () => (
   <section className="section other-projects">
@@ -24,7 +24,9 @@ const OtherResearch = () => (
         researchData.map((project) => (
           <div className="project">
             <div className="project__name">
-              <a href={project.url} target="_blank" rel="noreferrer" className="arrow-link">{project.name}</a>
+              <a href={project.url} target="_blank" rel="noreferrer" className="arrow-link">
+                {project.name.length > 50 ? project.name.substring(0, 50) + '...' : project.name}
+              </a>
             </div>
             <p>{Parser(project.description)}</p>
             <div className="project__used">
@@ -39,6 +41,6 @@ const OtherResearch = () => (
       }
     </div>
   </section>
-)
+);
 
-export default OtherResearch
+export default OtherResearch;
