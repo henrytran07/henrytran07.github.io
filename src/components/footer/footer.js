bfa1e69e34a4cb0cb0206d735a29a1f4ecbf9c01
Henry Tran <tloc1@student.mtsac.edu>
2025-09-25 14:26:32 -0700

    Add copyright

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -15,16 +15,20 @@ const socialData = [
     "url": "https://www.linkedin.com/in/henry-tran-8b6025314/"
   },
 ]
+
 const Footer = () => (
   <footer className="footer">
     <div className="footer__links">
       {
         socialData.map((social) => (
-          <a href={social.url} target="_blank" rel="noreferrer" title="{{link.title}}">
+          <a href={social.url} target="_blank" rel="noreferrer" title={social.title} key={social.title}>
             <span className="text">{social.title}</span>
           </a>
         ))
       }
+    </div>
+    <div className="footer__copyright">
+      <p>© 2025 <a href="https://henrytran07.github.io" target="_blank" rel="noreferrer">henrytran07.github.io</a></p>
     </div>
   </footer>
 )
