03c38ffaa18d3ef8a6f44f2f266c9509bf62e64b
Henry Tran <tloc1@student.mtsac.edu>
2025-09-25 18:07:11 -0700

    Add node-module

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,18 +1,23 @@
 import * as React from "react"
 import "./footer.scss"
+import "./footer.scss"
+import 'font-awesome/css/font-awesome.min.css'
 
 const socialData = [
   {
     "title": "email",
-    "url": "mailto:henrytran0715@gmail.com"
+    "url": "mailto:henrytran0715@gmail.com",
+    "icon": "fa-envelope"
   },
   {
     "title": "github",
-    "url": "https://github.com/henrytran07"
+    "url": "https://github.com/henrytran07",
+    "icon": "fa-github" 
   },
   {
     "title": "linkedin",
-    "url": "https://www.linkedin.com/in/henry-tran-8b6025314/"
+    "url": "https://www.linkedin.com/in/henry-tran-8b6025314/",
+    "icon": "fa-linkedin"
   },
 ]
 
@@ -21,8 +26,8 @@ const Footer = () => (
     <div className="footer__links">
       {
         socialData.map((social) => (
-          <a href={social.url} target="_blank" rel="noreferrer" title={social.title} key={social.title}>
-            <span className="text">{social.title}</span>
+          <a href={social.url} target="_blank" rel="noreferrer" key={social.title} title={social.title}>
+            <i className={`fa ${social.icon}`} style={{ fontSize: "24px", marginRight: "8px" }}></i> {/* Display icon */}
           </a>
         ))
       }
@@ -31,6 +36,6 @@ const Footer = () => (
       <p>©<a href="https://henrytran07.github.io" target="_blank" rel="noreferrer">henrytran07.github.io 2025</a></p>
     </div>
   </footer>
-)
+);
 
 export default Footer
