fe376646334de5505dd03ea2f82c266d5341a208
Henry Tran <tloc1@student.mtsac.edu>
2025-09-25 18:20:31 -0700

    Add all (2)

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,21 +1,22 @@
 import * as React from "react";
 import "./footer.scss";
+import 'font-awesome/css/font-awesome.min.css';
 
 const socialData = [
   {
     "title": "email",
     "url": "mailto:henrytran0715@gmail.com",
-    "icon": '/images/emojis/gmail_icon.png' // Assuming you downloaded email icon in SVG format
+    "icon": "fa-envelope" // FontAwesome email icon
   },
   {
     "title": "github",
     "url": "https://github.com/henrytran07",
-    "icon": '/images/emojis/github_icon.png'// Assuming you downloaded GitHub icon in SVG format
+    "icon": "fa-github" // FontAwesome GitHub icon
   },
   {
     "title": "linkedin",
     "url": "https://www.linkedin.com/in/henry-tran-8b6025314/",
-    "icon": '/images/emojis/linkedin_icon.png' // Assuming you downloaded LinkedIn icon in SVG format
+    "icon": "fa-linkedin" // FontAwesome LinkedIn icon
   },
 ];
 
@@ -25,7 +26,7 @@ const Footer = () => (
       {
         socialData.map((social) => (
           <a href={social.url} target="_blank" rel="noreferrer" key={social.title} title={social.title}>
-            <img src={`./assets/icons/${social.icon}`} alt={social.title} style={{ width: "24px", height: "24px", marginRight: "8px" }} />
+            <i className={`fa ${social.icon}`} style={{ fontSize: "24px", marginRight: "8px" }}></i> {/* Display icon */}
           </a>
         ))
       }
