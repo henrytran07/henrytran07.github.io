ab047b54725a73474dc890292d2ae692f632ca5f
Henry Tran <tloc1@student.mtsac.edu>
2025-09-26 19:31:46 -0700

    Fix

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -29,7 +29,6 @@ const Intro = () => {
 
   return <header className="intro">
     <h1 className="intro__hello">{introData.title}
-      <span className={isHovering ? "emoji wave-hand animated wave" : "emoji wave-hand animated"} onMouseOver={onMouseOver} onMouseOut={onMouseOut} onFocus={onMouseOver} onBlur={onMouseOut} role="button" tabIndex={0} aria-label="wave hand"></span>
     </h1>
 
     <img src={introData.image} alt="Henry Tran" className="intro__image" />
