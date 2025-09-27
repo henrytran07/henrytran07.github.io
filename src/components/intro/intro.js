8580efb546b7c387a50dc6fc3033ee064ab18248
Henry Tran <tloc1@student.mtsac.edu>
2025-09-27 13:20:02 -0700

    Add

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -3,7 +3,7 @@ import "./intro.scss";
 
 const introData = {
   title: "Hi! I'm Henry.",
-  beforeName: "It is nice to meet you",
+  beforeName: "IT'S NICE TO MEET YOU",
   image: '/images/emojis/henry-tran.JPG',
 };
 
@@ -30,7 +30,7 @@ const Intro = () => {
   return (
     <header className="intro">
       <h1 className="intro__hello">{introData.title}</h1>
-      <h2 className="intro__tagline">{introData.beforeName}</h2> {/* Make this bold */}
+      <h2 className="my_intro">{introData.beforeName}</h2> {/* Make this bold */}
       <img src={introData.image} alt="Henry Tran" className="intro__image" />
     </header>
   );
