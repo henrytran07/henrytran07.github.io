2d23c607b99e8e089b2941d20eb0c1d7c8c43930
Henry Tran <tloc1@student.mtsac.edu>
2025-09-27 00:33:41 -0700

    Try bvackground

--- src/components/switch/switch.js
+++ src/components/switch/switch.js
@@ -4,7 +4,7 @@ import "./switch.scss";
 const Switch = () => {
   React.useEffect(() => {
     // Always apply dark mode when the page loads
-    document.body.classList.add('night');
+    document.body.classList.add('black');
   }, []); // This effect runs only once when the component is mounted
 
   return null; // No need to render anything
