ee2d60e8415328555d1a09b3a6f63b5341523307
Henry Tran <tloc1@student.mtsac.edu>
2025-09-26 23:00:57 -0700

    Add

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,5 @@
-// App.js
-
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/index";  // Assuming this is your main page
 import Cv from "./Cv";  // Import the new CV page component
 import Navbar from "./components/navbar";  // Import your Navbar component
@@ -10,12 +8,11 @@ function App() {
   return (
     <Router>
       <Navbar /> {/* Navbar is now part of the routing structure */}
-      <Switch>
+      <Routes>
         {/* Define routes for different pages */}
-        <Route path="/" exact component={IndexPage} /> {/* Main page */}
-        <Route path="/cv" component={Cv} /> {/* CV page */}
-        {/* You can add more routes for other pages */}
-      </Switch>
+        <Route path="/" element={<IndexPage />} /> {/* Main page */}
+        <Route path="/cv" element={<Cv />} /> {/* CV page */}
+      </Routes>
     </Router>
   );
 }
