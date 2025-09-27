24d82c8ddf0daf167cb87fdf0cf75ff69082a699
Henry Tran <tloc1@student.mtsac.edu>
2025-09-26 23:12:21 -0700

    Add

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
-// App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Import React Router components
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing from React Router v6
 import IndexPage from "./pages/index"; // Your main page component
 import Cv from "./Cv"; // Your CV page component
 import Navbar from "./components/navbar"; // Your Navbar component
@@ -9,11 +8,11 @@ function App() {
   return (
     <Router basename="/henry-tran"> {/* Set the basename to the sub-directory */}
       <Navbar /> {/* Navbar is now part of the routing structure */}
-      <Switch>
+      <Routes>
         {/* Define routes for different pages */}
-        {/* <Route path="/" exact component={IndexPage} /> Main page */}
-        <Route path="/cv" component={Cv} /> {/* CV page */}
-      </Switch>
+        <Route path="/" element={<IndexPage />} /> {/* Main page */}
+        <Route path="/cv" element={<Cv />} /> {/* CV page */}
+      </Routes>
     </Router>
   );
 }
