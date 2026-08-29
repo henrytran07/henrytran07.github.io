d00e51bcf2c0da10ae954ec2fb6b503a8ffce1b3
Henry Tran <henrytran07@g.ucla.edu>
2026-08-29 09:05:13 -0700

    fix the navbar dimension and add mobile version

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,46 @@
-const links = document.querySelectorAll('.js-nav-link');
-const navbar = document.querySelector('.js-nav-bar');
+const links = document.querySelectorAll(".js-nav-link");
+
+const navbar = document.querySelector(".js-nav-bar");
 
 const aboutSection = document.getElementById("about");
 const experienceSection = document.getElementById("experience");
 const projectSection = document.getElementById("projects");
+const contactSection = document.getElementById("contacts");
 
 function clearSelected() {
-  links.forEach((l) => l.classList.remove('selected'));
+  links.forEach((link) => {
+    link.classList.remove("selected");
+  });
 }
-
 function selectLink(link) {
   clearSelected();
-  if (link) link.classList.add('selected');
+  if (link) {
+    link.classList.add("selected");
+  }
 }
 
+let isClicked = false;
+// CLICK
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    isClicked = true;
+    selectLink(link);
+    setTimeout(() => {
+      isClicked = false;
+    }, 800);
+  });
+});
+// SCROLL
 window.addEventListener("scroll", () => {
-  if (navbar) navbar.classList.toggle("scrolled", window.scrollY > 50);
 
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+  if (isClicked) {
+    return;
+  }
   const y = window.scrollY + 200;
-
-  if (projectSection && y >= projectSection.offsetTop) {
+  if (contactSection && y >= contactSection.offsetTop) {
+    selectLink(links[3]);
+  } else if (projectSection && y >= projectSection.offsetTop) {
     selectLink(links[2]);
   } else if (experienceSection && y >= experienceSection.offsetTop) {
     selectLink(links[1]);
